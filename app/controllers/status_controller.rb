4e98ef77a79392b0a3b2c2b4ed0e8e79617ea2ff
Daniel Velázquez <daniel545mjj@gmail.com>
2024-04-09 20:15:35 -0400

    MEM-26 avoid request authentication for status endpoint

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,4 +1,6 @@
 class StatusController < ApplicationController
+  skip_before_action :authenticate_request
+
   def index
     git_commit_hash = `git rev-parse --short HEAD`.strip
     render json: { status: 'API is up and running', commit: git_commit_hash }, status: :ok
