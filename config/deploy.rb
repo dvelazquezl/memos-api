455e23a4fff39a2f14609421c93052c41f976085
Daniel Velázquez <daniel545mjj@gmail.com>
2024-07-25 23:06:06 -0400

    add puma settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
-lock '~> 3.16.0'
+lock '~> 3.19.1'
 
 set :application, 'memos-backend'
-set :repo_url, 'git clone git@bitbucket.org:tfg-workspace/memos-backend.git'
+set :repo_url, 'git@bitbucket.org:tfg-workspace/memos-backend.git'
 
 set :deploy_to, '/var/www/tfg-api.ddns.net'
 
@@ -15,9 +15,34 @@ set :pty, true
 set :default_env, { path: '~/.rvm/bin:$PATH' }
 set :keep_releases, 5
 
-after 'deploy:publishing', 'deploy:restart'
+# Puma settings
+set :puma_threads, [4, 16]
+set :puma_workers, 0
+
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
+namespace :puma do
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
 namespace :deploy do
   task :restart do
     invoke 'puma:restart'
   end
 end
+
+after 'deploy:publishing', 'deploy:restart'
